5a76562ed302a1dc4defdabec9d9eb111c6a33c6
unknown <arafat.innotimes.net@gmail.com>
2025-07-18 00:58:00 +0600

    module 27 Done

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import userController from "./user.controller";
-import { createZodSchema } from "./user.validation";
+import { createZodSchema, updateZodSchema } from "./user.validation";
 import { validateRequest } from "../../middleware/validateRequest";
 import { checkAuth } from "../../middleware/checkAuth";
 import { UserRole } from "./user.interface";
@@ -11,8 +11,10 @@ const router = Router();
 
 
 router.post("/register", validateRequest(createZodSchema), userController.createUser);
+
 router.get("/all-users", checkAuth(UserRole.ADMIN, UserRole.SUPER_ADMIN), userController.getUser);
-router.patch("/:id", checkAuth(...Object.values(UserRole)) , userController.updateUser)
+
+router.patch("/:id", validateRequest(updateZodSchema), checkAuth(...Object.values(UserRole)), userController.updateUser)
 
 const userRoutes = router;
 export default userRoutes;
